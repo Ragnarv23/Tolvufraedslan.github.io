f6d5fb1be893b10de59ecc0936754f3ee36aa5fa
ragnarv23 <ragnarv23@ru.is>
2025-02-06 19:49:55 +0000

    fuck

--- app.py
+++ app.py
@@ -20,8 +20,8 @@ def index():
     return render_template('course_list/index.html', courses=courses)
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
+    #with app.app_context():
+    #    db.create_all()
     port = int(os.environ.get('PORT', 5000)) 
     app.run(debug=True, host='0.0.0.0', port=port)
 
